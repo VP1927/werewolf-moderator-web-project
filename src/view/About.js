8bf623bf15f730e60b05015665e2a17fb7683d7b
vannhi1118 <37665519+vannhi1118@users.noreply.github.com>
2019-07-08 13:15:47 +0700

    Update About

--- src/view/About.js
+++ src/view/About.js
@@ -3,10 +3,28 @@ import React from 'react';
 
 
 class About extends React.Component{
-	render () {
+	render() {
 		return (
-			<div>
-				<h1>Lulquit</h1>
+			<div className='container-fluid aboutPage'>
+				<div className='row'>
+					<div className='col-lg-4' id='f1_container'>
+						<div id="f1_card" class="shadow">
+							<div class="front face">
+								<img class=" img-fluid" src="/images/werewolf.jpg" />
+								{/* <h4 class="card-title">Sunlimetech</h4> */}
+							</div>
+							<div class="back face center">
+								<p>After playing werewolves boardgame for 5 years,
+							we realized the struggle of the moderator having to
+							remember all players' roles as well as their actions during the night time.
+							This website was built to support the moderator in terms of 
+							setting up gameplay and managing players.</p>
+							</div>
+						</div>
+					</div>
+
+
+				</div>
 			</div>
 		)
 	}
